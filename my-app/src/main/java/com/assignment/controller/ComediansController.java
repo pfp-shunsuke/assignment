715456a7386605a2058fddbef1b0e83fba908e6f
pfp-shunsuke <s.tada@pfp.co.jp>
2021-05-12 15:55:58 +0900

    privateを追記

--- my-app/src/main/java/com/assignment/controller/ComediansController.java
+++ my-app/src/main/java/com/assignment/controller/ComediansController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RequestMapping("/comedians")
 @RequiredArgsConstructor
 public class ComediansController {
+    private final ComedianService comedianService;
 
     final ComedianService comedianService;
 
